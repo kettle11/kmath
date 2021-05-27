8e938b4fcbb527358818a8005e9c35d3d7e1d44e
Ian Kettlewell <ian.kettlewell@gmail.com>
2021-05-27 11:30:56 -0400

    Vec3u types

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,9 @@ pub use quaternion::*;
 pub use vector::*;
 
 mod default_types {
+    use super::{Matrix, Quaternion, Vector};
     use crate::geometry::BoundingBox;
 
-    use super::{Matrix, Quaternion, Vector};
     pub type Vec2 = Vector<f32, 2>;
     pub type Vec3 = Vector<f32, 3>;
     pub type Vec4 = Vector<f32, 4>;
@@ -23,6 +23,10 @@ mod default_types {
     pub type Vec2i = Vector<i32, 2>;
     pub type Vec3i = Vector<i32, 3>;
     pub type Vec4i = Vector<i32, 4>;
+
+    pub type Vec2u = Vector<usize, 2>;
+    pub type Vec3u = Vector<usize, 3>;
+    pub type Vec4u = Vector<usize, 4>;
 
     pub type Mat3 = Matrix<f32, 3, 3>;
     pub type Mat4 = Matrix<f32, 4, 4>;
